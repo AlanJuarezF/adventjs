d68e2d5f7a89d7f5ae7586f8e3a55361fd3be2d6
Alan Juarez <alanwlfx@gmail.com>
2023-01-26 23:54:44 -0600

    retos

---
+++ reto6.js
@@ -0,0 +1,26 @@
+function createCube(size) {
+    let arriba = '';
+    let abajo = '';
+    let contador = 1;
+     for(let i = size; i>0; i--) {
+      arriba += ' '.repeat(i - 1);
+      arriba += "/\\".repeat(contador);
+      arriba += "_\\".repeat(size);
+      arriba += "\n";
+      contador++;
+     }
+     contador--;
+
+     for(let i = 0; i<size; i++) {
+      abajo += ' '.repeat(i);
+      abajo += "\\/".repeat(contador);
+      abajo += "_/".repeat(size);
+      if(i != size - 1) abajo += "\n";
+      contador--;
+     }
+    return arriba + abajo;
+  }
+
+  
+  
+  console.log(createCube(3));
